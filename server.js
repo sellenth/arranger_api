9ce1db125b7f67fa4b6c0547d9a7a08f77b680ac
Halston <sellenth@oregonstate.edu>
2020-02-16 21:59:07 -0800

    initial

---
+++ server.js
@@ -0,0 +1,40 @@
+// Create express app
+var express = require("express")
+var app = express()
+var db = require("./database.js")
+
+// Server port
+var HTTP_PORT = 8000 
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    console.log("Try getting http://localhost:8000/api/arrangements")
+});
+// Root endpoint
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
+
+app.get("/api/arrangements", (req, res, next) => {
+    var sql = "select * from arrangements"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        for (var i = 0; i < rows.length; i++){
+          rows[i].points = JSON.parse(rows[i].points);
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404);
+});
+
